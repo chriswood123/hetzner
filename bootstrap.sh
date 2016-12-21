89c2ef3bcac23fdf936d465b7306439be2d48091
Chris Wood <chris.wood@skyscanner.net>
2016-12-21 16:51:11 +0000

    Delete chef dpkg and run chef

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,3 +5,9 @@ echo "I hope you added SSH key(s) to authorized keys and disabled password-less 
 wget https://packages.chef.io/files/stable/chef/12.17.44/ubuntu/16.04/chef_12.17.44-1_amd64.deb
 
 dpkg -i chef_12.17.44-1_amd64.deb
+
+rm chef_12.17.44-1_amd64.deb
+
+cd chef
+
+chef-client --local-mode test.rb
